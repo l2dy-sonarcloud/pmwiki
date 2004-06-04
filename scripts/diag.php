994e63ea5361aaf60f2ffdad0b2fad2d0654404b
pmichaud <pmichaud@524c5546-5005-0410-9a3e-e25e191bd360>
2004-06-04 14:32:42 +0000

    *** empty log message ***
    git-svn-id: svn://pmwiki.org/trunk/pmwiki@13 524c5546-5005-0410-9a3e-e25e191bd360

---
+++ scripts/diag.php
@@ -0,0 +1,21 @@
+<?php if (!defined('PmWiki')) exit();
+/*  Copyright 2003-2004 Patrick R. Michaud (pmichaud@pobox.com)
+    This file is part of PmWiki; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published
+    by the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.  See pmwiki.php for full details.
+
+    This file adds "?action=diag" and "?action=phpinfo" actions to PmWiki.  
+    This produces lots of diagnostic output that may be helpful to the 
+    software authors when debugging PmWiki or other scripts.
+*/
+
+if ($action=='diag') {
+  header('Content-type: text/plain');
+  print_r($GLOBALS);
+  exit();
+}
+
+if ($action=='phpinfo') { phpinfo(); exit(); }
+
+?>
