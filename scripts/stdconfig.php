07ac705acb1326793471bc5c2d7d86525517c760
pmichaud <pmichaud@524c5546-5005-0410-9a3e-e25e191bd360>
2004-06-05 19:18:27 +0000

    Initial version.
    git-svn-id: svn://pmwiki.org/trunk/pmwiki@40 524c5546-5005-0410-9a3e-e25e191bd360

---
+++ scripts/stdconfig.php
@@ -0,0 +1,33 @@
+<?php if (!defined('PmWiki')) exit();
+/*  Copyright 2002-2004 Patrick R. Michaud (pmichaud@pobox.com)
+    This file is part of PmWiki; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published
+    by the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.  See pmwiki.php for full details.
+
+    This file allows features to be easily enabled/disabled in config.php.
+    Simply set variables for the features to be enabled/disabled in config.php
+    before including this file.  For example:
+        $EnableQAMarkup=0;                      #disable Q: and A: tags
+        $EnableDefaultWikiStyles=1;             #include default wikistyles
+    Each feature has a default setting, if the corresponding $Enable
+    variable is not set then you get the default.
+
+    To avoid processing any of the features of this file, set 
+        $EnableStdConfig = 0;
+    in config.php.
+*/
+
+SDV($DefaultPage,"$DefaultGroup.$DefaultName");
+if ($pagename=='') $pagename=$DefaultPage;
+
+if (!IsEnabled($EnableStdConfig,1)) return;
+
+if ($action=='diff' && @!$HandleActions['diff'])
+  include_once("$FarmD/scripts/pagerev.php");
+if (IsEnabled($EnableTemplateLayout,1))
+  include_once("$FarmD/scripts/tlayout.php");
+if (IsEnabled($EnableDiag,0)) 
+  include_once("$FarmD/scripts/diag.php");
+
+?>
