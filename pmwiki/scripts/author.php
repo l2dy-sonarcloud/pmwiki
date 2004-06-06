7a72867301cbbf7cae0e4ca3a3d462785e89635c
pmichaud <pmichaud@524c5546-5005-0410-9a3e-e25e191bd360>
2004-06-06 13:52:16 +0000

    Moved location of [[~...]] markup.
    git-svn-id: svn://pmwiki.org/trunk@48 524c5546-5005-0410-9a3e-e25e191bd360

--- pmwiki/scripts/author.php
+++ pmwiki/scripts/author.php
@@ -13,7 +13,7 @@ SDV($AuthorCookieDir,'/');
 SDV($AuthorGroup,'Profiles');
 SDV($AuthorRequiredFmt,
   "<h3 class='wikimessage'>$[An author name is required.]</h3>");
-$MarkupPatterns[4005]['/\\[\\[~(.*?)\\]\\]/'] =
+$MarkupPatterns[3050]['/\\[\\[~(.*?)\\]\\]/'] =
   "[[$AuthorGroup/$1]]";
 
 if (!isset($Author)) {
