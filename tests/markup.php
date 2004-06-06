a6a09978a4b901a56a28cc09e6874bab6ec1efcb
pmichaud <pmichaud@524c5546-5005-0410-9a3e-e25e191bd360>
2004-06-06 02:24:20 +0000

    Changed markup used for tests+results.
    git-svn-id: svn://pmwiki.org/trunk/pmwiki@44 524c5546-5005-0410-9a3e-e25e191bd360

--- tests/markup.php
+++ tests/markup.php
@@ -8,21 +8,19 @@ $HTMLStylesFmt[] = "
   .pass { background-color:#ddffdd; }
   .fail { background-color:#ffdddd; }";
 
-$MarkupPatterns[3000]["/^=test\\s+([\\s\\w]+?)\\s+$KeepToken(\\d+)$KeepToken/e"] =
-  "PZZ(\$tname='$1',\$tkeep1=$2)";
-$MarkupPatterns[3001]["/=result\\s+([\\s\\w]*)$KeepToken(\\d+)$KeepToken/e"] =
-  "Keep(TestResult(\$pagename,\$tname,\$GLOBALS['KPV'][\$tkeep1],
+$MarkupPatterns[3000]["/^=test\\s+([^$KeepToken]+?)$KeepToken(\\d+)$KeepToken(?:[^$KeepToken]*)$KeepToken(\\d+)$KeepToken/e"] =
+  "Keep(TestResult(\$pagename,PSS('$1'),\$GLOBALS['KPV'][$2],
     str_replace(array('&amp;','&lt;','&gt;'),array('&','<','>'),
-      \$GLOBALS['KPV'][$2])))";
+    \$GLOBALS['KPV'][$3])))";
 
 function TestResult($pagename,$testname,$testmarkup,$testresult) {
   $testmarkup = trim($testmarkup);
-  $testresult = trim($testresult)."\n";
+  $testresult = FmtPageName(trim($testresult)."\n",$pagename);
   $out = MarkupToHTML($pagename,$testmarkup);
   $pass = ($out==$testresult) ? 'pass' : 'fail';
   $x = "<table width='100%' border='1' cellspacing='0'>
     <tr><td colspan='2' class='$pass'><b>$pagename</b> - $testname: $pass</td></tr>";
-  if (!$pass || @$_SESSION['details'])
+  if ($pass!='pass' || @$_SESSION['details'])
     $x .= "<tr><td width='50%'><pre>$testmarkup</pre></td>
         <td><pre>".htmlspecialchars($out)."</pre></td></tr>
       <tr><td>$out</td>
