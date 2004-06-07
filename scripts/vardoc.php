5b79981ebddba26b358de71ad79fed89acea18bc
pmichaud <pmichaud@524c5546-5005-0410-9a3e-e25e191bd360>
2004-06-07 05:46:27 +0000

    Initial version from 0.6
    git-svn-id: svn://pmwiki.org/trunk/pmwiki@67 524c5546-5005-0410-9a3e-e25e191bd360

---
+++ scripts/vardoc.php
@@ -0,0 +1,59 @@
+<?php if (!defined('PmWiki')) exit();
+/*  Copyright 2002-2004 Patrick R. Michaud (pmichaud@pobox.com)
+    This file is part of PmWiki; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published
+    by the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.  See pmwiki.php for full details.
+
+    This script provides special handling for WikiWords that are
+    preceded by a $, treating them as PmWiki variables to be looked up
+    in the variable documentation pages if such documentation exists.
+    The $VarPagesFmt variable contains a list of pages to be searched
+    to build an index of the variable documentation.  This index is 
+    generated only once per browse request, and then only when needed.
+*/
+
+SDV($VarPagesFmt,array('PmWiki.Variables'));
+$MarkupPatterns[3550]["/\\$($WikiWordPattern)\\b/e"] =
+  "Keep(VarLink(\$pagename,'$1','$$1'))";
+$MarkupPatterns[2800]["/^:\\$($WikiWordPattern):/"] = 
+  ':[[#$1]]$$1:';
+$HTMLStylesFmt[] = "a.varlink { text-decoration:none; }\n";
+
+function VarLink($pagename,$tgt,$txt) {
+  global $VarIndex,$FmtV,$VarLinkMissingFmt,$VarLinkExistsFmt;
+  SDV($VarLinkMissingFmt,'$LinkText');
+  SDV($VarLinkExistsFmt,"<a class='varlink' href='\$LinkUrl'><code class='varlink'>\$LinkText</code></a>");
+  VarIndexLoad($pagename);
+  $FmtV['$LinkText'] = $txt;
+  $FmtV['$LinkUrl'] = @$VarIndex[$tgt]['url'];
+  if (@!$VarIndex[$tgt]['url'])
+    return FmtPageName($VarLinkMissingFmt,$pagename);
+  return FmtPageName($VarLinkExistsFmt,$pagename);
+}
+
+function VarIndexLoad($pagename) {
+  global $VarPagesFmt,$VarIndex,$WikiWordPattern;
+  static $loaded;
+  if ($loaded) return;
+  foreach($VarPagesFmt as $v) {
+    if (@$loaded[$v]) continue;
+    $vlist = array($v);
+    $t = ReadTrail($pagename,$v);
+    if ($t) 
+      for($i=0;$i<count($t);$i++) 
+        if (@!$loaded[$t[$i]['pagename']]) $vlist[]=$t[$i]['pagename'];
+    foreach($vlist as $vname) {
+      $vpage = ReadPage($vname); @$loaded[$vname]++;
+      if (!$vpage) continue;
+      if (!preg_match_all("/\n:\\$([[:upper:]]\\w+):/",$vpage['text'],$match))
+        continue;
+      foreach($match[1] as $n) {
+        $VarIndex[$n]['pagename'] = $vname;
+        $VarIndex[$n]['url'] = FmtPageName("\$PageUrl#$n",$vname);
+      }
+    }
+  }
+}
+
+?>
