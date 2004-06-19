6293c6d6ee1867be56e2e0bf55bf62a21e6339b6
pmichaud <pmichaud@524c5546-5005-0410-9a3e-e25e191bd360>
2004-06-19 10:54:49 +0000

    Changed SignAuthor to use ReplaceOnSave.
    git-svn-id: svn://pmwiki.org/trunk/pmwiki@79 524c5546-5005-0410-9a3e-e25e191bd360

--- scripts/author.php
+++ scripts/author.php
@@ -29,21 +29,12 @@ $k = MakePageName($pagename,$Author);
 SDV($AuthorPage,"$AuthorGroup/$k");
 SDV($AuthorLink,"[[~$Author]]");
 
-if (IsEnabled($EnableAuthorSignature,1)) 
-  array_unshift($EditFunctions,'SignAuthor');
+if (IsEnabled($EnableAuthorSignature,1)) {
+  $ROSPatterns['/~~~~/'] = '[[~$Author]] $CurrentTime';
+  $ROSPatterns['/~~~/'] = '[[~$Author]]';
+}
 if (IsEnabled($EnableAuthorRequired,0))
   array_unshift($EditFunctions,'RequireAuthor');
-
-## SignAuthor converts "~~~~" ($AuthorSignatureMarkup) into an author's
-## signature ("--[[~$Author]]") as part of editing/posting a page.
-function SignAuthor($pagename,&$page,&$new) {
-  global $Author,$AuthorSignatureFmt;
-  if (!@$_POST['post']) return;
-  SDV($AuthorSignatureMarkup,'~~~~');
-  SDV($AuthorSignatureFmt,'--[[~$Author]]');
-  $new['text'] = str_replace($AuthorSignatureMarkup,
-    FmtPageName($AuthorSignatureFmt,$pagename),$new['text']);
-}
 
 ## RequireAuthor forces an author to enter a name before posting.
 function RequireAuthor($pagename,&$page,&$new) {
