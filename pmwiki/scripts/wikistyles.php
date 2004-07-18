1e1f7e5e5115021b9ca154aee29e9cde02f2d86d
pmichaud <pmichaud@524c5546-5005-0410-9a3e-e25e191bd360>
2004-07-18 00:47:11 +0000

    Added block-level wikistyles.
    git-svn-id: svn://pmwiki.org/trunk@101 524c5546-5005-0410-9a3e-e25e191bd360

--- pmwiki/scripts/wikistyles.php
+++ pmwiki/scripts/wikistyles.php
@@ -15,4 +15,30 @@
 Markup('.class','directive',
   '/\\[:css\\s+(\\.\\w[-\\w]*\\s*\\{[^}]*})\\s*:\\]/e',
   "PZZ(\$GLOBALS['HTMLStylesFmt'][]=PSS('$1\n'))");
+
+# allow other CSS properties in WikiStyles
+$WikiStyleCSSPatterns[] = 'display|margin[-\\w]*|border[-\\w]*';
+
+# the 'block' style is a shortcut for apply=block
+SDV($WikiStyle['block'],array('apply'=>'block'));
+
+# some styles for justifying text
+foreach(array('center','left','right') as $k)
+  SDV($WikiStyle[$k],array('text-align'=>$k, 'align'=>'block'));
+
+# the 'newwin' style causes links to open in a new window
+SDV($WikiStyle['newwin'],array('target' => '_blank'));
+
+# the 'comment' style turns text into a comment via display:none; property
+$WikiStyleCSSPatterns[] = 'display';
+SDV($WikiStyle['comment'],array('display'=>'none'));
+
+# define standard color text styles from CSS color names
+foreach(array('black','white','red','yellow','blue','gray',
+  'silver','maroon','green', 'navy', 'purple') as $c)
+    SDV($WikiStyle[$c],array('color'=>$c));
+
+# example of defining a color from a #rrggbb spec
+SDV($WikiStyle['darkgreen'],array('color'=>'#006400'));
+
 ?>
