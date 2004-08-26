64e06abef6a432d1a9038bdce748e0a22547bc65
pmichaud <pmichaud@524c5546-5005-0410-9a3e-e25e191bd360>
2004-08-26 18:14:35 +0000

    Added missing copyright notice.
    git-svn-id: svn://pmwiki.org/trunk/pmwiki@127 524c5546-5005-0410-9a3e-e25e191bd360

--- scripts/wikistyles.php
+++ scripts/wikistyles.php
@@ -1,4 +1,10 @@
-<?php
+<?php if (!defined('PmWiki')) exit();
+/*  Copyright 2004 Patrick R. Michaud (pmichaud@pobox.com)
+    This file is part of PmWiki; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published
+    by the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.  See pmwiki.php for full details.
+*/
 
 ## %% markup
 Markup('%%','style','%','return ApplyStyles($x);');
