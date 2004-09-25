03c6d3746047986082992d35f5079ae30cc72429
pmichaud <pmichaud@524c5546-5005-0410-9a3e-e25e191bd360>
2004-09-25 06:06:49 +0000

    Initial version of 1.x compatibility.
    git-svn-id: svn://pmwiki.org/trunk@195 524c5546-5005-0410-9a3e-e25e191bd360

---
+++ pmwiki/scripts/compat1x.php
@@ -0,0 +1,102 @@
+<?php if (!defined('PmWiki')) exit();
+/*  Copyright 2004 Patrick R. Michaud (pmichaud@pobox.com)
+    This file is part of PmWiki; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published
+    by the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.  See pmwiki.php for full details.
+
+    This file attempts to ease conversions of PmWiki 1.x installations
+    to PmWiki 2.  This is definitely a preliminary implementation and
+    still needs a fair amount of work.
+
+    The major component is the PageStore1x, which allows pages
+    to be read from an existing PmWiki 1.x wiki.d/ directory, 
+    converting PmWiki 1 markup into PmWiki 2 markup as the page is read.  
+    Pages are then saved in the PmWiki 2 installation's wiki.d/
+    directory, which should be separate from the original wiki.d/.
+
+    The intent is that a wiki administrator can install, configure, and
+    test a PmWiki 2 installation on an existing set of PmWiki 1.x pages 
+    without losing or modifying the 1.x page files.
+
+    Here's a suggested sequence of steps:
+
+    1.  Install PmWiki 2 somewhere other than the location of
+        the existing PmWiki 1.x installation.  Make sure the new
+        installation works properly.
+
+    2.  In the config.php for the PmWiki 2 installation, add the line
+
+            array_push($WikiLibDirs,
+                new PageStore1x("path/to/pmwiki1/wiki.d/$PageName"));
+
+        where "path/to/pmwiki1/wiki.d" is the path to the wiki.d
+        directory of the PmWiki 1.x installation.  
+
+    The approach is that the PmWiki 2 installation will read pages from
+    the PmWiki 1 installation (and convert them to the new markups),
+    but save edited pages in the new markup to the new installation,
+    leaving the PmWiki 1 installation intact.
+
+    For more details, see 
+        http://www.pmwiki.org/pmwiki2/pmwiki.php/PmWiki/Compat1x
+    
+*/
+
+SDVA($Compat1x,array(
+  # noheader, nofooter, etc.
+  "/\\[\\[(noheader|nofooter|nogroupheader|nogroupfooter|notitle|spacewikiwords)\\]\\]/" => '[:$1:]',
+
+  # include, redirect
+  "/\\[\\[(include|redirect):(.*?)\\]\\]/" => '[:$1 $2:]',
+
+  # table, cell, cellnr, endtable
+  "/\\[\\[(table|cell|cellnr|tableend)(\\s.*?)?\\]\\]/" => '[:$1$2:]',
+
+  # [[$Title]]
+  "/\\[\\[\\\$Title\\]\\]/" => '{$Name}',
+
+  # [[$Group]], [[$Version]], etc.
+  "/\\[\\[\\$(Group|Version|Author|LastModified|LastModifiedBy|LastModifiedHost)\\]\\]/" => '{$$1}',
+
+  # [[$Edit text]], [[$Diff text]]
+  "/\\[\\[\\\$Edit\\s(.*?)\\]\\]/" => '[[{$Name}?action=edit |$1]]',
+  "/\\[\\[\\\$Diff\\s(.*?)\\]\\]/" => '[[{$Name}?action=diff |$1]]',
+
+  # [[$Search]], [[$SearchResults]], [[$Attachlist]]
+  "/\\[\\[\\\$Search\\]\\]/" => '[:searchbox:]',
+  "/\\[\\[\\\$Searchresults\\]\\]/" => '[:searchresults:]',
+  "/\\[\\[\\\$Attachlist(\\s.*?)?\\]\\]/" => '[:attachlist$1:]',
+
+  # [[target linktext]]
+  "/\\[\\[((\\w|\\#)[^$UrlExcludeChars\\s]*)\\s(.*?)\\]\\]/" => '[[$1 |$3]]',
+
+  # [[target]]
+  "/\\[\\[(\\w[^$UrlExcludeChars\\s]*)\\]\\]/" => '[[$1 |<#>]]',
+
+  # [[Group.{{free link}} link text]]
+  "/\\[\\[($GroupPattern([\\/.]))?\\{\\{(~?\\w[-\\w\\s.\\/]*)\\}\\}([-\\w]*)\\s(.*?)\\]\\]/" => '[[$1$3 |$5]]',
+
+  # [[Group.{{free link|s}} link text]]
+  "/\\[\\[($GroupPattern([\\/.]))?\\{\\{(~?\\w[-\\w\\s.\\/]*)\\|([-\\w\\s]*)\\}\\}([-\\w]*)\\s(.*?)\\]\\]/" => '[[$1$3$4 |$6]]',
+
+  # Group.{{free link}}ext
+  "/($GroupPattern([\\/.]))?\\{\\{(~?\\w[-\\w\\s.\\/]*)\\}\\}([-\\w]*)/" 
+    => '[[$1$3]]$4',
+
+  # Group.{{free link|s}}ext
+  "/($GroupPattern([\\/.]))?\\{\\{(~?\\w[-\\w\\s.\\/]*)\\|([-\\w\\s]*)\\}\\}([-\\w]*)/" => '[[$1$3($4)]]$5',
+));
+
+class PageStore1x extends PageStore {
+  function read($pagename) {
+    global $Compat1x;
+    $page = parent::read($pagename);
+    if ($page) 
+      $page['text'] = preg_replace(array_keys($Compat1x),
+        array_values($Compat1x), $page['text']);
+    return $page;
+  }
+}
+   
+?>
