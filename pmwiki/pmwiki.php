ed8f50530163a04b99dd662b77f195691219b327
pmichaud <pmichaud@524c5546-5005-0410-9a3e-e25e191bd360>
2004-11-26 00:51:32 +0000

    Changed default reporting level.
    git-svn-id: svn://pmwiki.org/trunk@356 524c5546-5005-0410-9a3e-e25e191bd360

--- pmwiki/pmwiki.php
+++ pmwiki/pmwiki.php
@@ -19,7 +19,7 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
-error_reporting(E_ALL);
+error_reporting(E_ALL ^ E_NOTICE);
 StopWatch('PmWiki');
 if (ini_get('register_globals')) 
   foreach($_REQUEST as $k=>$v) { unset(${$k}); }
