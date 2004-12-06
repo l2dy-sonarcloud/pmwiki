77fcb0e22aa79523cfa2791c04e358faec80c03c
pmichaud <pmichaud@524c5546-5005-0410-9a3e-e25e191bd360>
2004-12-06 20:24:08 +0000

    Updated for UTF-8 pagename support.
    git-svn-id: svn://pmwiki.org/trunk@416 524c5546-5005-0410-9a3e-e25e191bd360

--- pmwiki/scripts/xlpage-utf-8.php
+++ pmwiki/scripts/xlpage-utf-8.php
@@ -9,19 +9,48 @@
     pagenames.
 */
 
-  global $HTTPHeaders, $U8;
-  $HTTPHeaders[] = 'Content-type: text/html; charset=utf-8';
-  $U8 = 'u';
+global $HTTPHeaders, $U8;
+$HTTPHeaders[] = 'Content-type: text/html; charset=utf-8';
 
-  $Newline = "\262\262\262";
-  $KeepToken = "\263\263\263";
-  $LinkToken = "\376\376\376";
-  $pagename = $_GET['pagename'];
+$Newline = "\262\262\262";
+$KeepToken = "\263\263\263";
+$LinkToken = "\376\376\376";
+$pagename = $_GET['pagename'];
 
-  if (!$pagename &&
-        preg_match('!^'.preg_quote($_SERVER['SCRIPT_NAME'],'!').'/?([^?]*)!',
-            $_SERVER['REQUEST_URI'],$match))
-      $pagename = urldecode($match[1]);
-  $pagename = preg_replace('!/+$!','',$pagename);
+if (!$pagename &&
+      preg_match('!^'.preg_quote($_SERVER['SCRIPT_NAME'],'!').'/?([^?]*)!',
+          $_SERVER['REQUEST_URI'],$match))
+    $pagename = urldecode($match[1]);
+$pagename = preg_replace('!/+$!','',$pagename);
+
+$GroupPattern = '[\\w\\x80-\\xff]+(?:-[[\\w\\x80-\\xff]+)*';
+$NamePattern = '[\\w\\x80-\\xff]+(?:-[[\\w\\x80-\\xff]+)*';
+$MakePageNameFunction = 'MakeUTF8PageName';
+$WikiWordPattern = 
+  '[A-Z][A-Za-z0-9]*(?:[A-Z][a-z0-9]|[a-z0-9][A-Z])[A-Za-z0-9]*';
+
+## MakeUTF8PageName is used to convert a UTF-8 string into a valid pagename.
+## It assumes that PHP has been compiled with mbstring support.
+function MakeUTF8PageName($basepage,$x) {
+  global $PagePathFmt;
+  $PageNameChars = '-\\w\\x80-\\xff';
+  if (!preg_match('/(?:([^.\\/]+)[.\\/])?([^.\\/]+)$/',$x,$m)) return '';
+  $name = preg_replace("/[^$PageNameChars]+/", ' ', $m[2]);
+  $name = preg_replace('/(?<=^| )(.)/eu', "mb_strtoupper('$1','UTF-8')", $name);
+  $name = str_replace(' ', '', $name);
+  if ($m[1]) {
+    $group = str_replace(' ','',
+      preg_replace("/\\b(\\w)/e", "strtoupper('$1')",
+        preg_replace("/[^$PageNameChars]+/", ' ', $m[1])));
+    return "$group.$name";
+  }
+  foreach((array)$PagePathFmt as $pg) {
+    $pn = FmtPageName(str_replace('$1',$name,$pg),$basepage);
+    if (PageExists($pn)) return $pn;
+  }
+  $group=preg_replace('/[\\/.].*$/','',$basepage);
+  return "$group.$name";
+}
+
 
 ?>
