90ddaadb0cf035e316ba579aeb316e2e4dd8301c
pmichaud <pmichaud@524c5546-5005-0410-9a3e-e25e191bd360>
2004-12-06 13:50:01 +0000

    Fixed spelling of 'gray'.
    git-svn-id: svn://pmwiki.org/trunk/pmwiki@410 524c5546-5005-0410-9a3e-e25e191bd360

--- pmwiki.php
+++ pmwiki.php
@@ -139,7 +139,7 @@ $HTMLStylesFmt['pmwiki'] = "
   .vspace { margin-top:1.33em; }
   .indent { margin-left:40px; }
   .outdent { margin-left:40px; text-indent:-40px; }
-  a.createlinktext { text-decoration:none; border-bottom:1px dotted grey; }
+  a.createlinktext { text-decoration:none; border-bottom:1px dotted gray; }
   a.createlink { text-decoration:none; position:relative; top:-0.5em;
     font-weight:bold; font-size:smaller; border-bottom:none; }
   ";
