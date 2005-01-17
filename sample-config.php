608f005756d12ea902d46faf3c938916ca4440ba
pmichaud <pmichaud@524c5546-5005-0410-9a3e-e25e191bd360>
2005-01-17 17:36:08 +0000

    Added example of additional gui buttons.
    git-svn-id: svn://pmwiki.org/trunk/pmwiki@531 524c5546-5005-0410-9a3e-e25e191bd360

--- sample-config.php
+++ sample-config.php
@@ -38,7 +38,7 @@ $WikiTitle = 'PmWiki';
 # $DefaultPasswords['admin'] = crypt('secret');
 
 ##  PmWiki comes with graphical user interface buttons for editing;
-##  to enable these buttons, set $EnableGUIButtons to 1.
+##  to enable these buttons, set $EnableGUIButtons to 1.  
 # $EnableGUIButtons = 1;
 
 ##  If you want uploads enabled on your system, set $EnableUpload=1.
@@ -111,6 +111,28 @@ $WikiTitle = 'PmWiki';
 
 ##  If you want to have to approve links to external sites before they
 ##  are turned into links, uncomment the line below.  See PmWiki.UrlApprovals.
+##  Also, setting $UnapprovedLinkCountMax limits the number of unapproved
+##  links that are allowed in a page (useful to control wikispam).
 # include_once('scripts/urlapprove.php');
+# $UnapprovedLinkCountMax = 10;
 
+##  The following lines make additional editing buttons appear in the
+##  edit page for subheadings, lists, tables, etc.
+# $GUIButtons['h2'] = array(400, '\\n!! ', '\\n', '$[Heading]',
+#                     '$GUIButtonDirUrlFmt/h2.gif"$[Heading]"');
+# $GUIButtons['h3'] = array(402, '\\n!!! ', '\\n', '$[Subheading]',
+#                     '$GUIButtonDirUrlFmt/h3.gif"$[Subheading]"');
+# $GUIButtons['indent'] = array(500, '\\n->', '\\n', '$[Indented text]',
+#                     '$GUIButtonDirUrlFmt/indent.gif"$[Indented text]"');
+# $GUIButtons['outdent'] = array(510, '\\n-<', '\\n', '$[Hanging indent]',
+#                     '$GUIButtonDirUrlFmt/outdent.gif"$[Hanging indent]"');
+# $GUIButtons['ol'] = array(520, '\\n# ', '\\n', '$[Ordered list]',
+#                     '$GUIButtonDirUrlFmt/ol.gif"$[Ordered (numbered) list]"');
+# $GUIButtons['ul'] = array(530, '\\n* ', '\\n', '$[Unordered list]',
+#                     '$GUIButtonDirUrlFmt/ul.gif"$[Unordered (bullet) list]"');
+# $GUIButtons['hr'] = array(540, '\\n----\\n', '', '',
+#                     '$GUIButtonDirUrlFmt/hr.gif"$[Horizontal rule]"');
+# $GUIButtons['table'] = array(600,
+#                       '||border=1 width=80%\\n||!Hdr ||!Hdr ||!Hdr ||\\n||     ||     ||     ||\\n||     ||     ||     ||\\n', '', '', 
+#                     '$GUIButtonDirUrlFmt/table.gif"$[Table]"');
 ?>
