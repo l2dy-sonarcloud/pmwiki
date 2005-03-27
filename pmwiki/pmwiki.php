215cede936820901346cfb8b76cc8059a44695f1
pmichaud <pmichaud@524c5546-5005-0410-9a3e-e25e191bd360>
2005-03-27 16:56:55 +0000

    Added comment about comments.
    git-svn-id: svn://pmwiki.org/trunk@606 524c5546-5005-0410-9a3e-e25e191bd360

--- pmwiki/pmwiki.php
+++ pmwiki/pmwiki.php
@@ -18,6 +18,12 @@
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    ----
+    Note from Pm:  Trying to understand the PmWiki code?  Wish it had 
+    more comments?  If you want help with any of the code here,
+    write me at <pmichaud@pobox.com> with your question(s) and I'll
+    provide explanations (and add comments) that answer them.
 */
 error_reporting(E_ALL ^ E_NOTICE);
 StopWatch('PmWiki');
