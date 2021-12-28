6ce48006fb0b68bef7bf339c65d540329922ebb1
petko <petko@524c5546-5005-0410-9a3e-e25e191bd360>
2021-12-28 07:04:03 +0000

    PmSyntax trying to fix a sub-pixel sizing mismatch between #text and #htext (cont.).
    git-svn-id: svn://pmwiki.org/pmwiki/trunk@3888 524c5546-5005-0410-9a3e-e25e191bd360

--- pub/guiedit/pmwiki.syntax.js
+++ pub/guiedit/pmwiki.syntax.js
@@ -284,13 +284,13 @@
       if(! chk_hlt.checked) return;
       var tc = text.value;
       if(tc===lastTextContent) return;
+      lastTextContent = tc;
       
       var clone = htext.cloneNode(false);
       htext.parentNode.replaceChild(clone, htext);
       htext.innerHTML = PmHi(tc+'\n');
       textScrolled();
       htext.addEventListener('scroll', preScrolled);
-      lastTextContent = tc;
     }
     function textScrolled() {
       if(! chk_hlt.checked) return;
