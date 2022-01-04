1524a0a587923a861d3896992b48036395a99826
petko <petko@524c5546-5005-0410-9a3e-e25e191bd360>
2022-01-04 13:34:02 +0000

    PmSyntax group generic directives after forms
    git-svn-id: svn://pmwiki.org/pmwiki/trunk@3961 524c5546-5005-0410-9a3e-e25e191bd360

--- pub/guiedit/pmwiki.syntax.js
+++ pub/guiedit/pmwiki.syntax.js
@@ -113,10 +113,10 @@
     ['ws3', '!meta', /(%\w[-\w]*)(.*?)(%)/g],
 
     // directives, forms
-    ['dir0', '*directive', /\(:\w[-\w]* *:\)/g],
     ['form', '!directive>keyword', /(\(:input[^\S\r\n]+)(\S.*?)(:\))/g,
       /^((pm)?form|text(area)?|radio|checkbox|select|email|tel|number|default|submit|reset|hidden|password|search|url|date|datalist|file|image|reset|button|e_\w+|captcha|end)/],
-    ['dir1', '!directive', /(\(:\w[-\w]*)(.*?)(:\))/g],
+    ['dir0', '*directive', /\(:\w[-\w]* *:\)/g], // simple
+    ['dir1', '!directive', /(\(:\w[-\w]*)(.*?)(:\))/g], // with attributes
 
     // inline
     ['link', 'punct', /(\[\[[\#!~]?|([#+][^\S\r\n]*)?\]\])/g], // link
