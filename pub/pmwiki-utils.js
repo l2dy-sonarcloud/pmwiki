473bcd3e0effc9fcffa134b9fc708e6e6079f1cd
petko <petko@524c5546-5005-0410-9a3e-e25e191bd360>
2022-06-07 14:34:51 +0000

    PmSyntax hopefully to work with older browsers (documentation only, not edit form) (cont.).
    git-svn-id: svn://pmwiki.org/pmwiki/trunk@4103 524c5546-5005-0410-9a3e-e25e191bd360

--- pub/pmwiki-utils.js
+++ pub/pmwiki-utils.js
@@ -513,3 +513,26 @@
   if( document.readyState !== 'loading' ) ready();
   else window.addEventListener('DOMContentLoaded', ready);
 })();
+
+/*
+ Element.closest() polyfill
+ https://developer.mozilla.org/en-US/docs/Web/API/Element/closest#polyfill
+ */
+if (!Element.prototype.matches) {
+  Element.prototype.matches =
+    Element.prototype.msMatchesSelector ||
+    Element.prototype.webkitMatchesSelector;
+}
+
+if (!Element.prototype.closest) {
+  Element.prototype.closest = function(s) {
+    var el = this;
+    do {
+      if (Element.prototype.matches.call(el, s)) return el;
+      el = el.parentElement || el.parentNode;
+    } while (el !== null && el.nodeType === 1);
+    return null;
+  };
+}
+
+
