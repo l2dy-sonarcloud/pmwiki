dbcb636d67485cb10516482f078884067f9d9ace
petko <petko@524c5546-5005-0410-9a3e-e25e191bd360>
2023-04-05 09:49:25 +0000

    Improve inline diff for rare cases.
    git-svn-id: svn://pmwiki.org/pmwiki/trunk@4424 524c5546-5005-0410-9a3e-e25e191bd360

--- scripts/pagerev.php
+++ scripts/pagerev.php
@@ -260,7 +260,7 @@ function DiffPrepareInline($x) {
   global $DiffSplitInlineDelims;
   SDV($DiffSplitInlineDelims, "-@!?#$%^&*()=+[]{}.'\"\\:|,<>_/;~");
   return preg_split("/([".preg_quote($DiffSplitInlineDelims, '/')."\\s])/", 
-    $x, -1, PREG_SPLIT_DELIM_CAPTURE);
+    "$x ", -1, PREG_SPLIT_DELIM_CAPTURE);
 }
 
 SDV($WordDiffFunction, 'PHPDiff'); # faster than sysdiff for many calls
